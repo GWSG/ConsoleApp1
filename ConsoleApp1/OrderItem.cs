19758d57450780ff1b7cbc43fb17dc3b54c227b5
GWSG <144191410+GWSG@users.noreply.github.com>
2023-09-11 16:08:33 +0800

    Update OrderItem.cs
    改過的

--- ConsoleApp1/OrderItem.cs
+++ ConsoleApp1/OrderItem.cs
@@ -1,15 +1,15 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace zhuang_c_sharp
 {
     internal class OrderItem
     {
         public int Index { get; set; }
-        public int Price { get; set; }
-        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public int subtotal {  get; set; }
     }
 }
