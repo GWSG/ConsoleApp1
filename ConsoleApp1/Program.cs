d0951243df25920399d7c4a6e06e0fc10ad2fd49
LaoYangWingChing <haoyongxu70@gmail.com>
2023-09-14 10:57:57 +0800

    文字修改2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -80,7 +80,7 @@ namespace ConsoleApp1
                 
                   if (index < 1 || index >=6)
                     {
-                        Console.WriteLine("沒有這個編號，請輸入目前佔有的編號");
+                        Console.WriteLine("沒有這個編號，請輸入目前已出現的編號");
                         continue;
                     }
                 
