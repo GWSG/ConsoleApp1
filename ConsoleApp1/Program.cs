fdf0a8b0ea67915bc8bd9687932fc7049da5239d
LaoYangWingChing <haoyongxu70@gmail.com>
2023-09-14 10:54:16 +0800

    修改文字敘述2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -80,7 +80,7 @@ namespace ConsoleApp1
                 
                   if (index < 1 || index >=6)
                     {
-                        Console.WriteLine("沒有這個編號，請輸入0-6的編號");
+                        Console.WriteLine("沒有這個編號，請輸入編號上有的數字");
                         continue;
                     }
                 
